66d9722930a385891910bedf8c01bb323d0ec71a
cecstar <yocecia@gmail.com>
2016-10-11 01:14:13 +0000

    updated part2 exercise

--- requestClient.js
+++ requestClient.js
@@ -1,25 +1,36 @@
 var request = require("request");
 var url = require("url");
 
-function readHTML (urlImport, callback) {
-
-  var body = "";
-
-  var requestedData = {
-    host: url,
-    path: urlImport
-};
-
-request.get(requestedData, (response) => {
-  //response.setEncoding("utf8");
-  //response.on("data", function(data) {
-  //  console.log("Data chunk received, line 12: ", data.length);
+function readHTML (url, print) {
+  request(url, function(err, response, body) {
+    if(err) {
+      throw err;
+  }
+  print(body);
   });
- // response.on("end", function() {
-  console.log("End! Response Stream Completed.");
-  };
+}
+function printHTML(htmlData) {
+    console.log(htmlData);
 }
 
-readHTML("www.example.com", function printHTML (htmlData) {
-  console.log(htmlData);
-});
+readHTML("http://www.example.com", printHTML);
+
+//   var body = "";
+
+//   var requestOptions = {
+//     host: url,
+//     path: "/"
+//   };
+// }
+//     request.get(requestOptions, (response) => {
+
+
+//     response.on("data", function(data) {
+//       body += data;
+//     });
+
+//     response.on("end", function() {
+//       console.log(body);
+//     });
+//   });
+// }
